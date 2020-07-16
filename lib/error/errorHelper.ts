0476ef94d88d4e6bbc33b032a808354967873ef3
Pedro Nascimento <pedro.nascimento@bitcapital.com.br>
2020-07-16 16:42:55 -0300

    chore: add documentation to stripStacks function

--- lib/error/errorHelper.ts
+++ lib/error/errorHelper.ts
@@ -19,6 +19,18 @@ function _stripStacksInternal(obj: any, clone: boolean): any {
   return output;
 }
 
+/**
+ * Recursively traverses an object and removes all stack properties in-place.
+ * @param obj The error or object to be stripped of stacks
+ */
 export function stripStacks<T extends any>(obj: T): T {
+  /*
+   * The second parameter controls wheter we return a new object or mutate the object in-place.
+   * If we choose to return a new object (which we should, since mutating parameters may lead to unpredictable
+   * consequences as the parameter object may live on and be passed to other places, and is generally frowned upon)
+   * we deep-clone the input object using fclone and mutate our clone, but fclone is intended for POJOs and does not
+   * preserve the prototype chain, so while this function was designed with the intention of not mutating its
+   * parameters, we are more scared of breaking the error prototype chain, so we mutate the object in place.
+   */
   return _stripStacksInternal(obj, false);
 }
