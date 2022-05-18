a496e50ad9fa10274e6b91abe3c730cb1f5198c9
Henrique Yoshio Shiraiwa <shiraiwa@spinpay.com.br>
2022-05-18 11:21:17 -0300

    feat: add newrelic transaction name for routes

--- dist/types/components/middlewares/async.d.ts
+++ dist/types/components/middlewares/async.d.ts
@@ -1,3 +1,3 @@
-import { Request, Response } from 'express';
-declare const asyncMiddleware: (functions: Function | Function[]) => ((req: Request, res: Response, next: any) => Promise<any>)[];
+import { Request, Response } from "express";
+declare const asyncMiddleware: (method: string, route: string, functions: Function | Function[]) => ((req: Request, res: Response, next: any) => Promise<any>)[];
 export default asyncMiddleware;
--- lib/components/middlewares/async.ts
+++ lib/components/middlewares/async.ts
@@ -1,17 +1,25 @@
-import * as util from 'util';
-import { Request, Response } from 'express';
+import * as util from "util";
+import { Request, Response } from "express";
 
-const asyncMiddleware = (functions: Function | Function[]) => {
+const asyncMiddleware = (method: string, route: string, functions: Function | Function[]) => {
   let fns = functions;
-  
+
   // Ensure input as an array
   if (!util.isArray(fns)) {
     fns = [fns];
   }
+
+  if (process.env.NEWRELIC_ENABLED === "true") {
+    const newRelicRouteNameMiddleware = (req: Request, res: Response, next: any) => {
+      require("newrelic").setTransactionName(`${method} ${route}`);
+      next();
+    };
+    fns = [newRelicRouteNameMiddleware, ...fns];
+  }
   // Map the array of filters and controllers with a Promise wrapper for express error handling
   return fns.map(fn => (req: Request, res: Response, next: any) => {
     if (!util.isFunction(fn)) {
-      let msg = 'Async middleware cannot wrap something that is not a function, got ' + typeof fn + '';
+      let msg = "Async middleware cannot wrap something that is not a function, got " + typeof fn + "";
       if (util.isString(fn)) {
         msg = `${msg}: "${fn}"`;
       }
